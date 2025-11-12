8cae04db3cecbc634aa59cc094893fcfcd97a524
atn <atn832@gmail.com>
2025-11-12 15:03:22 +0900

    share instance of TodoProvider (#5)

--- lib/todo_list_widget.dart
+++ lib/todo_list_widget.dart
@@ -2,29 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:my_todo_app/todo.dart';
 import 'package:my_todo_app/todo_provider.dart';
 
-class TodoListWidget extends StatefulWidget {
-  const TodoListWidget({super.key});
+class TodoListWidget extends StatelessWidget {
+  const TodoListWidget(this.todoProvider, {super.key});
 
-  @override
-  State<StatefulWidget> createState() {
-    return TodoListWidgetState();
-  }
-}
-
-class TodoListWidgetState extends State<TodoListWidget> {
-  late final TodoProvider _provider;
-
-  @override
-  void initState() {
-    _provider = TodoProvider();
-    _provider.open('todos.db');
-    super.initState();
-  }
+  final TodoProvider todoProvider;
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Todo>>(
-      stream: _provider.list(),
+      stream: todoProvider.list(),
       builder: (context, snapshot) {
         print(snapshot.data);
         if (snapshot.connectionState == ConnectionState.waiting) {
