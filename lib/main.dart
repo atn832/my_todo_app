d3c95362823a40017454140cb2034ad701cbbda9
Anh Tuan <atn832@gmail.com>
2025-11-12 15:01:19 +0900

    share instance of TodoProvider

--- lib/main.dart
+++ lib/main.dart
@@ -7,32 +7,55 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MainAppState();
+  }
+}
+
+class MainAppState extends State<MainApp> {
+  late final TodoProvider _todoProvider;
+  late final Future<dynamic> _open;
+
+  @override
+  void initState() {
+    _todoProvider = TodoProvider();
+    _open = _todoProvider.open('todos.db');
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                final todo = Todo()
-                  ..id = null
-                  ..title = 'Write tests'
-                  ..done = false;
-                final todoProvider = TodoProvider();
-                await todoProvider.open('todos.db');
-                final savedTodo = await todoProvider.insert(todo);
-                print(savedTodo.id);
-                final readTodo = await todoProvider.getTodo(savedTodo.id!);
-                print(readTodo.title);
-              },
-              child: Text('Create a todo'),
-            ),
-            Expanded(child: TodoListWidget()),
-          ],
+        body: FutureBuilder(
+          future: _open,
+          builder: (context, asyncSnapshot) {
+            if (asyncSnapshot.connectionState != ConnectionState.done) {
+              return CircularProgressIndicator();
+            }
+            return Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    final todo = Todo()
+                      ..id = null
+                      ..title = 'Write tests'
+                      ..done = false;
+                    final savedTodo = await _todoProvider.insert(todo);
+                    print(savedTodo.id);
+                    final readTodo = await _todoProvider.getTodo(savedTodo.id!);
+                    print(readTodo.title);
+                  },
+                  child: Text('Create a todo'),
+                ),
+                Expanded(child: TodoListWidget(_todoProvider)),
+              ],
+            );
+          },
         ),
       ),
     );
