436be84977fdbfe9007ccb9dd699612617ba9b59
Anh Tuan <atn832@gmail.com>
2025-11-12 19:19:06 +0900

    wrote tests for MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,56 +3,39 @@ import 'package:my_todo_app/todo.dart';
 import 'package:my_todo_app/todo_list_widget.dart';
 import 'package:my_todo_app/todo_provider.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  // This line is necessary to properly initialize the sqflite database.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final todoProvider = TodoProvider();
+  await todoProvider.open('todos.db');
+
+  runApp(MainApp(todoProvider));
 }
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp(this.todoProvider, {super.key});
 
-  @override
-  State<StatefulWidget> createState() {
-    return MainAppState();
-  }
-}
-
-class MainAppState extends State<MainApp> {
-  late final TodoProvider _todoProvider;
-  late final Future<dynamic> _open;
-
-  @override
-  void initState() {
-    _todoProvider = TodoProvider();
-    _open = _todoProvider.open('todos.db');
-    super.initState();
-  }
+  final TodoProvider todoProvider;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: FutureBuilder(
-          future: _open,
-          builder: (context, asyncSnapshot) {
-            if (asyncSnapshot.connectionState != ConnectionState.done) {
-              return CircularProgressIndicator();
-            }
-            return Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    final todo = Todo()
-                      ..id = null
-                      ..title = 'Write tests'
-                      ..done = false;
-                    await _todoProvider.insert(todo);
-                  },
-                  child: Text('Create a todo'),
-                ),
-                Expanded(child: TodoListWidget(_todoProvider)),
-              ],
-            );
-          },
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                final todo = Todo()
+                  ..id = null
+                  ..title = 'Write tests'
+                  ..done = false;
+                await todoProvider.insert(todo);
+              },
+              child: Text('Create a todo'),
+            ),
+            Expanded(child: TodoListWidget(todoProvider)),
+          ],
         ),
       ),
     );
