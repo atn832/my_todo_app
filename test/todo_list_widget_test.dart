c39b004f7ff56302686b2636902ebff340e7d920
atn <atn832@gmail.com>
2025-11-12 18:52:02 +0900

    test TodoListWidget with one item (#14)
    * test list with one item
    
    * expose test failure artifacts
    
    * always upload artifacts
    
    * run even if tests fail
    
    * run workflow on macos

--- test/todo_list_widget_test.dart
+++ test/todo_list_widget_test.dart
@@ -4,10 +4,15 @@ import 'package:my_todo_app/todo_list_widget.dart';
 import 'package:my_todo_app/todo_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
+import 'todo_provider_test.dart';
+
 main() {
   // Init ffi loader to use the in-memory database.
   sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
+  // When using testWidgts, we have to use `databaseFactoryFfiNoIsolate`
+  // instead of `databaseFactoryFfi`.
+  // https://github.com/tekartik/sqflite/issues/841#issuecomment-1200859788
+  databaseFactory = databaseFactoryFfiNoIsolate;
 
   late TodoProvider p;
 
@@ -27,11 +32,21 @@ main() {
       MaterialApp(home: Scaffold(body: TodoListWidget(p))),
     );
     await tester.pump();
-    expect(find.text('Do something'), findsNothing);
 
     await expectLater(
       find.byType(TodoListWidget),
       matchesGoldenFile('goldens/todo_list_widget/empty.png'),
     );
+    expect(find.text('Do something'), findsNothing);
+
+    // Add a todo.
+    await p.insert(makeTodo());
+    await tester.pump();
+
+    await expectLater(
+      find.byType(TodoListWidget),
+      matchesGoldenFile('goldens/todo_list_widget/oneTodo.png'),
+    );
+    expect(find.text('Do something'), findsOneWidget);
   });
 }
