6806b26cd985c3a2f5fc79a99988d9bc5ef7f147
Anh Tuan <atn832@gmail.com>
2025-11-13 10:08:33 +0900

    fix for unit tests

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// This file sets up fonts for all tests in the folder. This way, screenshot
+// tests will render actual text instead of boxes.
 // https://api.flutter.dev/flutter/flutter_test/GoldenFileComparator-class.html#including-fonts
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   setUpAll(() async {
@@ -13,5 +15,9 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async {
     await fontLoader.load();
   });
 
+  // Necessary for tests that do not use testWidgets eg unit tests like
+  // todo_provider_test. Without it, we get a `Binding has not yet been
+  // initialized.` error.
+  TestWidgetsFlutterBinding.ensureInitialized();
   await testMain();
 }
