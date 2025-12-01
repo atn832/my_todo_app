9eac786a6bc52eab7f493a9c6e59c4c23fcf45c5
atn <atn832@gmail.com>
2025-12-01 14:52:21 +0900

    screenshot tests: render icons correctly (#34)

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/services.dart';
+import 'package:alchemist/alchemist.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // This file sets up fonts for all tests in the folder. This way, screenshot
@@ -8,11 +8,10 @@ import 'package:flutter_test/flutter_test.dart';
 // https://api.flutter.dev/flutter/flutter_test/GoldenFileComparator-class.html#including-fonts
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   setUpAll(() async {
-    final Future<ByteData> font = rootBundle.load(
-      'assets/Roboto-VariableFont_wdth,wght.ttf',
-    );
-    final FontLoader fontLoader = FontLoader('Roboto')..addFont(font);
-    await fontLoader.load();
+    // Use Alchemist to load the fonts listed in FontManifest.json:
+    // * Roboto from the assets folder
+    // * MaterialIcons
+    await loadFonts();
   });
 
   // Necessary for tests that do not use testWidgets eg unit tests like
